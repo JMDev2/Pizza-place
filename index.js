53e3565b6bb2490d40c427b107cae3727207f940
JOSEPHMAINA1995 <mainajoseph964@gmail.com>
2022-03-18 20:18:27 +0300

    ft:This will add the the button properties

--- index.js
+++ index.js
@@ -16,3 +16,15 @@ $(document).ready(function(){
         $(".text2").hide();
       });
   });
+
+//   capturing the values
+
+$('button#submit').click(function(){
+    var pizzaSize = $('#size :selected').text();
+    var pizzaCrust = $('#crust :selected').text();
+    var pizzaTopping = $("#toppings").val();
+    
+    console.table(`${pizzaSize} ${pizzaCrust} ${pizzaTopping}`)
+
+   
+});
